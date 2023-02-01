4eb5ddf0c100bef0025d957abbc1301fe5ef48a9
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-02-01 02:10:02 +0100

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 const markup = [];
