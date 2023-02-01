a460e856a963aea3b30415c13dab7f6c5e1ca604
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-02-01 10:20:30 +0100

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 const markup = [];
 const galleryListEl = document.querySelector('.gallery');
